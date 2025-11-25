e1ac3557ec40f40ba1a0cfffcef897374e484152
hwang min <minhwang7201@naver.com>
2025-11-26 07:42:15 +0900

    Update cover and countdown sections

--- src/components/sections/CountdownSection.tsx
+++ src/components/sections/CountdownSection.tsx
@@ -11,7 +11,7 @@ export default function CountdownSection() {
   })
 
   useEffect(() => {
-    const weddingDate = new Date('2025-11-08T13:00:00+09:00')
+    const weddingDate = new Date('2026-04-11T13:00:00+09:00')
 
     const calculateTimeLeft = () => {
       const now = new Date()
